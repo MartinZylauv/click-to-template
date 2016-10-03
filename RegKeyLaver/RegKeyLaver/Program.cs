0ca122a10acbddc4867c32ef4328e6af5194a410
MZK <zyllelol@gmail.com>
2016-10-03 15:01:36 +0200

    Added the two test projects

---
+++ RegKeyLaver/RegKeyLaver/Program.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RegKeyLaver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RegistryKey rKey = Registry.ClassesRoot.OpenSubKey(@"Directory\shell", true);
+            String[] names = rKey.GetSubKeyNames();
+            foreach (String s in names)
+            {
+                Console.WriteLine(rKey.ToString());
+                Console.ReadLine();
+            }
+            RegistryKey newKey = rKey.CreateSubKey("Tester");
+           
+            RegistryKey newSubKey = newKey.CreateSubKey("command");
+            newSubKey.SetValue("", @"C:\Users\MZK\Desktop\TestAfRegShortcut.exe " + "\"" + "%1" + "\"");
+            String[] namess = rKey.GetSubKeyNames();
+            foreach (String s in namess)
+            {
+                Console.WriteLine(s);
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+
+            newSubKey.Close();
+            newKey.Close();
+            rKey.Close();
+        }
+    }
+}
---
+++ directoryfinder/directoryfinder/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace directoryfinder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IntPtr MyHwnd = FindWindow(null, "Directory");
+            var t = Type.GetTypeFromProgID("Shell.Application");
+            dynamic o = Activator.CreateInstance(t);
+            try
+            {
+                var ws = o.Windows();
+                for (int i = 0; i < ws.Count; i++)
+                {
+                    var ie = ws.Item(i);
+                    if (ie == null || ie.hwnd != (long)MyHwnd) continue;
+                    var path = System.IO.Path.GetFileName((string)ie.FullName);
+                    if (path.ToLower() == "explorer.exe")
+                    {
+                        var explorepath = ie.document.focuseditem.path;
+                    }
+                }
+            }
+            finally
+            {
+                Marshal.FinalReleaseComObject(o);
+            }
+
+        }
+    }
+}
